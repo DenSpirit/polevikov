4eec0e32137343634fb543749a4bd294bb0ee508
DSP <elfmax@tut.by>
2016-10-26 02:05:48 +0300

    simple makefile, omega calc stub

--- code/main.c
+++ code/main.c
@@ -1,3 +1,6 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
 int N;
 int NODEC;
 double EPS;
@@ -40,108 +43,150 @@ void printm(double** A,char name[]){
 #endif //VERBOSE
 }
 
-int main(int argc, char* argv[])
-{
+double** create() {
+    int size = N;
+    double** m = calloc(size, sizeof(double*));
+    int i;
+    for(i=0;i<size;i++) {
+        m[i] = calloc(size,sizeof(double));
+    }
+    return m;
+}
+
+void drop(double** m, int size) {
+    int i;
+    for(i=0;i<size;i++) {
+        free(m[i]);
+    }
+    free(m);
+}
+
+typedef struct state state;
+struct state {
     double Gr;
     double Pr;
+    double** psi;
+    double** omega;
+    double** T;
+};
+
+double middle(double** m, int i, int j, char pm, char dir) {
+    int iindex = dir == 'x' ? 1 : 0;
+    int jindex = dir == 'y' ? 1 : 0;
+    double f = (m[i+iindex][j+jindex]-m[i-iindex][j-jindex])/(2*H);
+    if (pm == '-' && f < 0 || pm == '+' && f > 0) {
+        return f;
+    } else {
+        return 0;
+    }
+}
+
+double calc_psi(int i, int j, state s) {
+    return (H*H * s.omega[i][j] + s.psi[i-1][j] + s.psi[i+1][j] 
+            + s.psi[i][j-1] + s.psi[i][j+1])/4.;
+}
+
+double calc_omega(int i, int j, state s) {
+    double im1= 
+    double ip1= 
+    double jm1= 
+    double jp1= 
+    double ij_coeff = (4/(H*H*s.Pr) + middle(s.psi,i,j,'+','y')/H
+                    + middle(s.psi,i,j,'+','x')/H
+                    - middle(s.psi,i,j,'-','y')/H
+                    - middle(s.psi,i,j,'-','x')/H);
+    return (im1+ip1+jm1+jp1)/ij_coeff;
+    return 0;
+}
+
+double calc_T(int i, int j, state s) {
+    double im1= s.T[i-1][j]*(1/(s.Pr*H*H) + 1/H * middle(s.psi, i, j, '+', 'y'));
+    double ip1= s.T[i+1][j]*(1/(s.Pr*H*H) - 1/H * middle(s.psi, i, j, '-', 'y'));
+    double jm1= s.T[i][j-1]*(1/(s.Pr*H*H) - 1/H * middle(s.psi, i, j, '-', 'x'));
+    double jp1= s.T[i][j+1]*(1/(s.Pr*H*H) + 1/H * middle(s.psi, i, j, '+', 'x'));
+    double ij_coeff = (4/(H*H*s.Pr) + middle(s.psi,i,j,'+','y')/H
+                    + middle(s.psi,i,j,'+','x')/H
+                    - middle(s.psi,i,j,'-','y')/H
+                    - middle(s.psi,i,j,'-','x')/H);
+    return (im1+ip1+jm1+jp1)/ij_coeff;
+}
+
+void border_psi(state s) {
+    int i;
+    for(i=0;i<N;i++) {
+        s.psi[i][0] = 0;
+        s.psi[i][1] = 0;
+        s.psi[0][i] = 0;
+        s.psi[1][i] = 0;
+        s.psi[i][NODEC] = 0;
+        s.psi[i][NODEC-1] = 0;
+        s.psi[NODEC][i] = 0;
+        s.psi[NODEC-1][i] = 0;
+    }
+}
+
+void border_omega(state s) {
+    int i;
+    for(i=0;i<N;i++) {
+        s.omega[0][i] = - s.omega[1][i] / 2. - 3./(H*H) * s.psi[1][i];
+        s.omega[NODEC][i] = - s.omega[NODEC-1][i] / 2. - 3./(H*H) * s.psi[NODEC-1][i];
+        s.omega[i][0] = - s.omega[i][1] / 2. - 3./(H*H) * s.psi[i][1];
+        s.omega[i][NODEC] = - s.omega[i][NODEC-1] / 2. - 3./(H*H) * s.psi[i][NODEC-1];
+    }
+}
+
+void border_T(state s) {
+    int i;
+    for(i=0;i<N;i++) {
+        s.T[i][0] = 0;
+        s.T[0][i] = 0;
+        s.T[NODEC][i] = sin(M_PI*i*H);
+        s.T[i][NODEC] = 0;
+    }
+}
+
+void output(double** m) {
+    int i,j;
+    for(i=0;i<=NODEC;i++) {
+        for(j=0;j<=NODEC;j++) {
+            printf("%lf %lf %lf\n", (double) i*H, (double) j*H, m[i][j]);
+        }
+    }
+}
+
+
+int main(int argc, char* argv[])
+{
     //sscanf(argv[1],"%d",&NODEC);
-    NODEC = 1000;
+    NODEC = 20;
     N = NODEC + 1;
     H = 1. / ((double) NODEC);
     EPS = H*H;
-    sscanf(argv[2],"%lf",&Gr);
-    sscanf(argv[3],"%lf",&Pr);
-    if(P<0)
-    {
-        P = fabs(P);
-    }
-    if (fabs(alpha) > 180)
-    {
-        return 1; // no periodical support
-    }
-    else
-    {
-        alpha = M_PI*alpha/180.; //to %pi notation
-    }
-    //first generation
-    double* z = gen_z0();
-    double* r = gen_r0();
-    printv(r,"r0");
-    printv(z,"z0");
-    //'previous'
-    double* z0 = calloc(N,sizeof(double));
-    double* r0 = calloc(N,sizeof(double));
-    //constant coeff tri-vectors for sweep method
-    double** Ar = gen_Ar();
-    double** Az = gen_Az();
+    state s;
+    sscanf(argv[1],"%lf",&(s.Gr));
+    sscanf(argv[2],"%lf",&(s.Pr));
+    s.psi = create();
+    s.omega = create();
+    s.T = create();
+    
+    border_psi(s);
+    border_T(s);
+    border_omega(s);
 
-    double *Fz,*Fr;
-    Fz=calloc(N,sizeof(double));
-    Fr=calloc(N,sizeof(double));
-    int k = 1;
-    double diff_r;
-    double diff_z;
-    double I1, I2;
-    do
-    {
-#ifdef VERBOSE
-        printf("\nk=%d\n\n",k++);
-#endif
-        I1 = gen_I1(z,r);
-        I2 = gen_I2(alpha, r);
-        printd(I1,"I1");
-        double Q = gen_Q(z,r,I1,I2,P,A,alpha);
-        printd(Q,"Q");
-        memcpy(z0,z,sizeof(double)*N);
-        memcpy(r0,r,sizeof(double)*N);
-        gen_Fz(z0,r0,P,A,alpha,I1,Q,Fz);
-        sweep(Az,Fz, z);
-        int i;
-        for(i = 0;i < N; i++) {
-            z[i] = z[i] * W + (1. - W) * z0[i];
+    int i;
+    int j;
+    for(i=1;i<NODEC-1;i++) {
+        for(j=1;j<NODEC-1;j++) {
+            s.psi[i][j] = calc_psi(i, j, s);
+            s.omega[i][j] = calc_omega(i, j, s);
+            s.T[i][j] = calc_T(i, j, s);
         }
-        gen_Fr( z,r0,P,A,alpha,I1,Q,Fr);
-        sweep(Ar,Fr, r);
-        for(i = 0;i < N; i++) {
-            r[i] = r[i] * W + (1. - W) * r0[i];
-        }
-        sanity("r", r);
-        sanity("z", z);
-        printv(r,"r");
-        printv(z,"z");
-        diff_r = vec_difference_norm(r,r0);
-        diff_z = vec_difference_norm(z,z0);
-#ifdef VERBOSE
-        printf("diff_z: %lf\ndiff_r: %lf\n",diff_z,diff_r);
-#endif
     }
-    while (diff_r > EPS || diff_z > EPS);
-    int i=0;
-    fprintf(stderr, "I:%lf\n", I1);
-    double I13 = cbrt(I1);
-#ifdef LATEXOUT
-    printf("r\tz\t\n");
-#endif
-    for(;i<N;i++){
-#ifdef LATEXOUT
-        printf("%lf\t%lf\n", r[i]/I13, z[i]/I13);
-#else
-        printf("%lf\t%lf\t%lf\n", i*H, r[i]/I13, z[i]/I13);
-#endif
-    }
-    free(z);
-    free(r);
-    free(z0);
-    free(r0);
-    free(Fz);
-    free(Fr);
-    free(Ar[0]);
-    free(Ar[1]);
-    free(Ar[2]);
-    free(Ar);
-    free(Az[0]);
-    free(Az[1]);
-    free(Az[2]);
-    free(Az);
+    output(s.T);
+
+    drop(s.psi, N);
+    drop(s.omega, N);
+    drop(s.T, N);
+
     return 0;
 }
